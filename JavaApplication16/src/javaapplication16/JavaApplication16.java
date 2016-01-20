63000984474aa670ee04898849bfb6649a9083e4
jorge <jorge@192.168.1.36>
2016-01-20 18:27:15 +0100

    prueva 1

---
+++ JavaApplication16/src/javaapplication16/JavaApplication16.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication16;
+
+/**
+ *
+ * @author jorge
+ */
+public class JavaApplication16 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hola Dr.mundo");
+        int numero;
+    }
+    
+}
