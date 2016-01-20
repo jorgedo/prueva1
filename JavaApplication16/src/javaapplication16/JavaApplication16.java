1db92a4a8973cbe481a15e97ba388ea4958b463c
jorge <jorge@192.168.1.36>
2016-01-20 18:40:24 +0100

    prueva 1 de la prueva

--- JavaApplication16/src/javaapplication16/JavaApplication16.java
+++ JavaApplication16/src/javaapplication16/JavaApplication16.java
@@ -15,7 +15,7 @@ public class JavaApplication16 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("hola Dr.mundo");
+        System.out.println("hola mión");
         int numero;
     }
     
